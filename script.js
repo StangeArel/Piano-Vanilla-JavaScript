bf2359f4ead4f02fc320f332ec950c7045bb2189
Valeriya Stange <valeriya.stange@gmail.com>
2025-06-24 16:03:47 +0200

    Tasteneffekte animiert und Eventlistener für Tastenanschläge hinzugefügt

--- script.js
+++ script.js
@@ -37,3 +37,12 @@ function removeTransition(e) {
     this.classList.remove("playing");
 }
 
+function hintsOn(e, index) {
+    e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+}
+
+hints.forEach(hintsOn);
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playNote);
